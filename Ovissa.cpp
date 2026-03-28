0adfdddd7ff2f35c04339ce59a989d4f8989977b
ellen825250055 <ellen.825250055@stu.untar.ac.id>
2026-03-28 19:18:23 +0700

    Add files via upload

---
+++ Ovissa.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[100001];
+
+    printf("masukan u yang diinginkan : ");
+    scanf("%s", s);
+
+    printf("%d\n", strlen(s));
+
+    return 0;
+}
