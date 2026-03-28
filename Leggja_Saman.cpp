11269de6a11391e60ba91e3b0ca7051b7778f136
ellen825250055 <ellen.825250055@stu.untar.ac.id>
2026-03-28 18:59:57 +0700

    Add files via upload

---
+++ Leggja_Saman.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, m;
+
+    printf("masukan nilai n : \n");
+    scanf("%d", &n);
+    printf("masukan nilai m : \n");
+    scanf("%d", &m);
+
+    printf("%d\n", n + m);
+
+    return 0;
+}
